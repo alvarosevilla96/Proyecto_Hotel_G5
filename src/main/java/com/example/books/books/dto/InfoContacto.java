268f15b94e9640feadfd7af6ae5c3471c9219593
alvarosevilla96 <alvarof.sev.96@gmail.com>
2025-05-13 19:07:03 +0200

    interconexion plantillasinlogin

---
+++ src/main/java/com/example/books/books/dto/InfoContacto.java
@@ -0,0 +1,14 @@
+package com.example.books.books.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class InfoContacto {
+    private String nombre;
+    private String email;
+
+
+
+}
