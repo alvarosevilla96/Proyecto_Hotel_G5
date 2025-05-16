456d106f1561d58cd02fc1d620fd2c2ed4ede08a
Joseajierro <jrmar0805@gmail.com>
2025-05-16 16:52:56 +0200

    push nuevas clases

--- src/main/java/com/example/books/books/controller/HotelesController.java
+++ src/main/java/com/example/books/books/controller/HotelesController.java
@@ -1,8 +1,11 @@
 package com.example.books.books.controller;
 
+import com.example.books.books.dto.InfoContacto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HotelesController {
@@ -42,7 +45,17 @@ public class HotelesController {
     }
 
     @GetMapping("/contact")
-    public String mostrarPaginaContact( ) {
+    public String mostrarPaginaContact(ModelMap intefrazConPantalla) {
+        InfoContacto contacto = new InfoContacto();
+        contacto.setNombre("El lobo feroz");
+        intefrazConPantalla.addAttribute("datoscontacto", contacto);
         return "contact";
+    }
+
+    @PostMapping("/contact")
+    public String postMostrarPaginaContact(@ModelAttribute(name="datoscontacto") InfoContacto infoContacto) {
+        System.out.println(infoContacto.getNombre());
+
+        return "home";
     }
 }
