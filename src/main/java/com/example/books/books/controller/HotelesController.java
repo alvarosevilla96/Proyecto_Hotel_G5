117bbb674977e5c6e88d263a30729d2def7bc137
Joseajierro <jrmar0805@gmail.com>
2025-05-20 17:45:56 +0200

    loogin test

--- src/main/java/com/example/books/books/controller/HotelesController.java
+++ src/main/java/com/example/books/books/controller/HotelesController.java
@@ -3,6 +3,7 @@ package com.example.books.books.controller;
 import com.example.books.books.dto.HotelDto;
 import com.example.books.books.dto.HotelsData;
 import com.example.books.books.dto.InfoContacto;
+import com.example.books.books.dto.LoginDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -31,8 +32,23 @@ public class HotelesController {
     }
 
     @GetMapping("/login2")
-    public String msotrarLogin( ){
+    public String msotrarLogin(Model model ){
+
+        LoginDto login = new LoginDto();
+        login.setEmail("<EMAIL>");
+        login.setPassword("<PASSWORD>");
+
+        model.addAttribute("email", login.getEmail());
+        model.addAttribute("pass", login.getPassword());
+
         return "login2";
+    }
+
+    @PostMapping("/login2")
+    public String postMostrarLogin(@ModelAttribute(name="datosLogin") LoginDto login ){
+        System.out.println(login.getEmail());
+        System.out.println(login.getPassword());
+        return "home";
     }
 
     @GetMapping("/crear-cuenta")
