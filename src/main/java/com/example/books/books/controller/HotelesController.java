c75af9a1e7b696d05e8b95ff0ee9ae972e5ab9dd
Joseajierro <jrmar0805@gmail.com>
2025-05-23 13:06:37 +0200

    controller

--- src/main/java/com/example/books/books/controller/HotelesController.java
+++ src/main/java/com/example/books/books/controller/HotelesController.java
@@ -1,5 +1,7 @@
 package com.example.books.books.controller;
 
+import com.example.books.books.dto.HotelDto;
+import com.example.books.books.dto.HotelsData;
 import com.example.books.books.dto.InfoContacto;
 import com.example.books.books.dto.LoginDto;
 import org.springframework.stereotype.Controller;
@@ -8,6 +10,8 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class HotelesController {
@@ -51,7 +55,10 @@ public class HotelesController {
     }
 
     @GetMapping("/hoteles")
-    public String mostrarHoteles( ){
+    public String mostrarHoteles(Model model){
+        List<HotelDto> hotels = HotelsData.hotelsDataList();
+        model.addAttribute("hotels", hotels);
+
         return "hoteles_web";
     }
 
