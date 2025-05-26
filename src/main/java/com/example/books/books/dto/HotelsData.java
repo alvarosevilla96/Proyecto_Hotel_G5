0711f73cd4d179057f6d966fc5390b6d7cf9fd09
Joseajierro <jrmar0805@gmail.com>
2025-05-26 09:50:18 +0200

    Arreglos hoteles web

--- src/main/java/com/example/books/books/dto/HotelsData.java
+++ src/main/java/com/example/books/books/dto/HotelsData.java
@@ -18,7 +18,7 @@ public class HotelsData {
                 "https://picsum.photos/600/400?random=5",
 
                 List.of("Montañas pequeñas", "Escalada divertida", "Helicoptero privado"),
-                "#"
+                "https://maps.googleapis.com/maps/api/staticmap?center=40.416775,-3.703790&zoom=14&size=600x400&markers=color:red%7C40.416775,-3.703790&key=AIzaSyCVai7Nwhhyg1XZVYa5ErxR_iRF0gzafGI"
         ));
 
         list.add(new HotelDto(
@@ -30,7 +30,7 @@ public class HotelsData {
                 "https://picsum.photos/600/400?random=2",
 
                 List.of("Playa a 15 minutos", "Piscina monstruosa", "Barco propio"),
-                "#"
+                "https://maps.googleapis.com/maps/api/staticmap?center=48.856613,2.352222&zoom=14&size=600x400&markers=color:red%7C48.856613,2.352222&key=AIzaSyCVai7Nwhhyg1XZVYa5ErxR_iRF0gzafGI"
 
         ));
 
@@ -43,7 +43,7 @@ public class HotelsData {
                 "https://picsum.photos/600/400?random=7",
 
                 List.of("No tenemos playa", "No tenemos piscina", "Tenemos un estanco cerca"),
-                "#"
+                "https://maps.googleapis.com/maps/api/staticmap?center=34.052235,-118.243683&zoom=14&size=600x400&markers=color:red%7C34.052235,-118.243683&key=AIzaSyCVai7Nwhhyg1XZVYa5ErxR_iRF0gzafGI"
 
         ));
 
