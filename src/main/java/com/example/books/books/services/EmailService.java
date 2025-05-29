13e670c29e33b450e88b8daa23c78a635f184038
alvarosevilla96 <alvarof.sev.96@gmail.com>
2025-05-29 16:39:37 +0200

    Configuración de servicio de envío de emails

---
+++ src/main/java/com/example/books/books/services/EmailService.java
@@ -0,0 +1,12 @@
+package com.example.books.books.services;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailService {
+
+}
